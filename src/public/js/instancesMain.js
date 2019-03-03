6fb3982a58566756579faa39f3aea032a5464599
Luke Chu <lukechu10@gmail.com>
2019-03-03 14:56:13 -0800

    Initial commit

--- src/public/js/instancesMain.js
+++ src/public/js/instancesMain.js
@@ -1,3 +1,36 @@
 var generateCmd = async(options={}) => {
-  
+
 }
+
+var updateVersionsListAPI = () => {
+  let settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://launchermeta.mojang.com/mc/game/version_manifest.json",
+    "method": "GET",
+    "headers": {
+      "cache-control": "no-cache"
+    }
+  }
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    versionList.set(response);
+  });
+}
+
+var getVersionInfo  = async(version) => {
+  if (this.versionsInfos[version])
+    return this.versionsInfos[version]
+  let versionsList = await AssetsDownload.getVersionsList()
+  const versionInfos = versionsList.versions.find(({id}) => id === version)
+  const url = versionInfos['url']
+  let path = this.path + '/versions/' + version + '/' + version + '.json'
+  let exists = fs.existsSync(path)
+  if (!exists) {
+    await writeToFile(path, url)
+  }
+  return this.versionsInfos[version] = JSON.parse(await fs.readFileSync(path))
+}
+
+updateVersionsListAPI();
